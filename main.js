95c7fb62baaf596af4c1bdd3c25e42f1ebf98cab
Sachin <sachin.patel9966@gmail.com>
2020-11-04 18:14:15 +0000

    flip works

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-// ------------- CLICK FUNCTION --------------------//
-const cards = document.querySelectorAll('.card');
+// // ------------- CLICK FUNCTION --------------------//
+// const cards = document.querySelectorAll('.card');
 
-const turnCard = () => {
-    console.log("clicked");
+// const turnCard = () => {
+//     console.log("clicked");
+// }
+
+// // for each card attach an event listener which listens for the 'click'
+// // whenever that event is triggered execute the function turnCard
+// cards.forEach(card => card.addEventListener('click', turnCard)); 
+
+// ---------- TURN CARD ----------------//
+const cards = document.querySelectorAll(".card");
+
+const turnMemoryCard = (element) => {
+  element.classList.toggle('turn');
+
+};
+cards.forEach(memoryCard => memoryCard.addEventListener("click", () => {
+    turnMemoryCard(memoryCard); 
+})); 
+
+// ----------- MATCH CARD -----------------//
+
+const matchCard = () => {
+    if (front.dataset.framework === back.dataset.framework){
+        
+    }
+
 }
 
-// for each card attach an event listener which listens for the 'click'
-// whenever that event is triggered execute the function turnCard
-cards.forEach(card => card.addEventListener('click', turnCard)); 
