ab8efe0a6418e6f32c76045872465dbe9f929885
Sachin <sachin.patel9966@gmail.com>
2020-11-05 22:53:40 +0000

    working-functions

--- main.js
+++ main.js
@@ -1,31 +1,92 @@
-// // ------------- CLICK FUNCTION --------------------//
-// const cards = document.querySelectorAll('.card');
 
-// const turnCard = () => {
-//     console.log("clicked");
-// }
+// -------- working ------------- //
 
-// // for each card attach an event listener which listens for the 'click'
-// // whenever that event is triggered execute the function turnCard
-// cards.forEach(card => card.addEventListener('click', turnCard)); 
-
-// ---------- TURN CARD ----------------//
 const cards = document.querySelectorAll(".card");
+let cardOne = null  // these are empty objects which will input values later and compare
+let cardTwo = null  
 
 const turnMemoryCard = (element) => {
   element.classList.toggle('turn');
 
 };
-cards.forEach(memoryCard => memoryCard.addEventListener("click", () => {
-    turnMemoryCard(memoryCard); 
-})); 
+cards.forEach(memoryCard => memoryCard.addEventListener("click", ()  => { // anonymous function 
+    turnMemoryCard(memoryCard);
+    if (!cardOne) {
+      cardOne = memoryCard; // if cardOne is not equal to null - input memoryCard into object do same for card two 
+      // then once both inputs from CLICKS have stored data inside each object - compare classes inside
+      
+    } 
+    else if (!cardTwo) {
+      cardTwo = memoryCard;
+      if (cardOne.childNodes[3].classList[1] === cardTwo.childNodes[3].classList[1]) {
+        console.log("WOW you picked two cards next to each other, congrats, are you proud?");    // if cardOne class === cardTwo class log yay and move to next
 
-// ----------- MATCH CARD -----------------//
 
-const matchCard = () => {
-    if (front.dataset.framework === back.dataset.framework){
+      } else {
+        let tempCardOne = cardOne;  // so by replicating cardOne and cardTwo i can flip the cards BEFORE
+        let tempCardTwo = cardTwo;  // javascript returns cardOne and cardTwo to null
+        window.setTimeout (() => {
+          turnMemoryCard(tempCardOne);
+          turnMemoryCard(tempCardTwo); //if cardOne === cardTwo run turn function on the temporary copies of cardOne and cardTwo
+        },1000); 
+
+        // turnMemoryCard(cardOne);   // when these are called after setting the window timeout
+        // turnMemoryCard(cardTwo);   // javascript returns the cards to object NULL before the turnMemoryCardFunction because animation is too slow and javascript works ultra fast basically
         
-    }
+      }
+      cardOne = null;
+      cardTwo = null;
+  }
+  })); 
+// --------------- ^^^^working^^^^ ------------------ //
 
-}
+  // .childNodes[3].classList[1]
+    // console.log(memoryCard.childNodes[3].classList[1]);
+    // } else {
+    //   if (cardOne === cardTwo) {
+    //     console.log("yay");
+    //     cardOne = "";
+    //     cardTwo = "";
+    //   }
+    // }
+    
 
+//     const cards = document.querySelectorAll(".card");
+// let cardOne = "";
+// let cardTwo = "";
+// const turnMemoryCard = (element) => {
+//   element.classList.toggle('turn');
+
+// };
+// cards.forEach(memoryCard => memoryCard.addEventListener("click", () => {
+//     turnMemoryCard(memoryCard);
+
+//     if (cardOne === "") {
+//       cardOne = memoryCard.childNodes[3].classList[1]
+//       ;
+//     } 
+//     else if (cardTwo === "") {
+//     cardTwo = memoryCard.childNodes[3].classList[1]
+    
+//     }
+//       else cardOne !== cardTwo; {
+//         console.log("yay");
+      
+//       } 
+//       //  else {
+//       //   turnMemoryCard();
+//       // }
+// })); 
+
+
+    
+  
+
+
+
+
+
+
+
+
+  
